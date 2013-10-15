1c6c1db9e799e0e946c1c0e89accd7251b5c8ab6
mateo <mateo@mint>
2013-10-14 19:31:19 -0500

    modalidades bean

---
+++ src/java/ManageBean/ModalidadBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManageBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author mateo
+ */
+@ManagedBean
+@RequestScoped
+public class ModalidadBean {
+
+    /**
+     * Creates a new instance of ModalidadBean
+     */
+    public ModalidadBean() {
+    }
+}
