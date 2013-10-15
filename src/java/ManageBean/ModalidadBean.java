93ddd61017808de81b8da615592d61e49f4d2fce
mateo <mateo@mint>
2013-10-14 19:58:26 -0500

    Lista Modalidades Completa

--- src/java/ManageBean/ModalidadBean.java
+++ src/java/ManageBean/ModalidadBean.java
@@ -4,20 +4,80 @@
  */
 package ManageBean;
 
+import facadePojo.DepartamentoFacade;
+import facadePojo.ModalidadFacade;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import pojo.Modalidad;
 
 /**
  *
  * @author mateo
  */
-@ManagedBean
+@ManagedBean(name="modalidadBean")
 @RequestScoped
 public class ModalidadBean {
+    
+    private List<Modalidad>listaMadalidades;
+    private List<Modalidad>listaModalidadesSeleccionda;
+    private Modalidad modalidadSeleccionada;
+    @EJB
+    DepartamentoFacade departamentoFacade;
+    @EJB
+    ModalidadFacade modalidadFacade;
+    private String clase="Modalidad";
+
+    public List<Modalidad> getListaMadalidades() {
+        return listaMadalidades;
+    }
+
+    public void setListaMadalidades(List<Modalidad> listaMadalidades) {
+        this.listaMadalidades = listaMadalidades;
+    }
+
+    public List<Modalidad> getListaModalidadesSeleccionda() {
+        return listaModalidadesSeleccionda;
+    }
+
+    public void setListaModalidadesSeleccionda(List<Modalidad> listaModalidadesSeleccionda) {
+        this.listaModalidadesSeleccionda = listaModalidadesSeleccionda;
+    }
+
+    public Modalidad getModalidadSeleccionada() {
+        return modalidadSeleccionada;
+    }
+
+    public void setModalidadSeleccionada(Modalidad modalidadSeleccionada) {
+        this.modalidadSeleccionada = modalidadSeleccionada;
+    }
+
+    public DepartamentoFacade getDepartamentoFacade() {
+        return departamentoFacade;
+    }
+
+    public void setDepartamentoFacade(DepartamentoFacade departamentoFacade) {
+        this.departamentoFacade = departamentoFacade;
+    }
+
+    public ModalidadFacade getModalidadFacade() {
+        return modalidadFacade;
+    }
+
+    public void setModalidadFacade(ModalidadFacade modalidadFacade) {
+        this.modalidadFacade = modalidadFacade;
+    }
+    
 
     /**
      * Creates a new instance of ModalidadBean
      */
     public ModalidadBean() {
+    }
+    @PostConstruct
+    public void reset(){
+        listaMadalidades=this.modalidadFacade.findAll();
     }
 }
