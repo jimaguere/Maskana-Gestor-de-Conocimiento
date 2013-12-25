86a0e8e132c4d8275522223c41450e2d80c3a7ac
mateo <mateo@mint>
2013-12-25 16:50:31 -0500

    Plantilla Buscador

---
+++ src/java/clases/Autor.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author mateo
+ */
+public class Autor{
+    private String nombre;
+    private String calificacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(String calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+}
