3568c6943b6101e9fcf13a29fce16f2e80b3aa9b
mateo <mateo@mint>
2014-01-12 20:19:32 -0500

    Searcher Api

--- src/java/ManageBean/ModeloBean.java
+++ src/java/ManageBean/ModeloBean.java
@@ -7,9 +7,6 @@ package ManageBean;
 
 import clases.Ontologia;
 import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.OntModelSpec;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.util.FileManager;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -19,6 +16,15 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.LMDirichletSimilarity;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
 
 
 /**
@@ -31,6 +37,8 @@ public class ModeloBean {
     OntModel modelOnt;
     String prefijo;
     private String ruta;
+    private FSDirectory dir;
+    private IndexSearcher searcher;
 
     public String getRuta() {
         return ruta;
@@ -40,7 +48,6 @@ public class ModeloBean {
         this.ruta = ruta;
     }
     
-
     public String getPrefijo() {
         return prefijo;
     }
@@ -56,6 +63,24 @@ public class ModeloBean {
     public void setModelOnt(OntModel modelOnt) {
         this.modelOnt = modelOnt;
     }
+
+    public FSDirectory getDir() {
+        return dir;
+    }
+
+    public void setDir(FSDirectory dir) {
+        this.dir = dir;
+    }
+
+    public IndexSearcher getSearcher() {
+        return searcher;
+    }
+
+    public void setSearcher(IndexSearcher searcher) {
+        this.searcher = searcher;
+    }
+    
+    
     public void guardarModelo(OntModel modelo) throws FileNotFoundException, IOException {
         File file = new File(ruta + "resources/owl/OntologiaIntancia.owl");
 //Hay que capturar las Excepciones
@@ -73,11 +98,14 @@ public class ModeloBean {
     }
     
     @PostConstruct
-    public void reset(){
+    public void reset() throws IOException{
         ServletContext servContx = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
         ruta = (String) servContx.getRealPath("/");
         Ontologia ont=new Ontologia();
         modelOnt=ont.iniciar(ruta);
         prefijo="http://www.owl-ontologies.com/TesisGrado.owl#";
+        dir = FSDirectory.open(new File(ruta+"resources/Indice"));
+        searcher= new IndexSearcher(IndexReader.open(dir));
+        searcher.setSimilarity(new LMDirichletSimilarity((float)1)); 
     }
 }
