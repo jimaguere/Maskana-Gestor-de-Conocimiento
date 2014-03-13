f3234b5c634e63490cb32aa345ed16016e323a9f
mateo <mateo@debian>
2014-03-13 10:20:10 -0500

    Directores de tesis

---
+++ src/java/clases/Director.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author mateo
+ */
+public class Director {
+    private String nombres;
+    private String apellidos;
+    
+    public Director(){
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+}
